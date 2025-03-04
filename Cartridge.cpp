1804d50dd6b1c7a6308a2ded85a4bbdd66c2ac18
UBC Student <chancelp@students.cs.ubc.ca>
2025-03-03 17:01:20 -0800

    actually fix compiler errors

--- Cartridge.cpp
+++ Cartridge.cpp
@@ -8,3 +8,7 @@
 #include "Cartridge.hpp"
 
 Cartridge::Cartridge() {};
+
+u8 Cartridge::read(u16 location) { return location & 0xff; };
+u8 Cartridge::readPPU(u16 location) { return location & 0xff; };
+void Cartridge::write(u16 location, u8 value) {};
